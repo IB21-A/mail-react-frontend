b7f6e80822217c5f22f9279e17d1e208cda1287e
Thom Zolghadr <thom@thomcodes.com>
2022-05-06 15:12:16 -0700

    Update Mailbox to show recipient in sent mailbox mode

--- src/components/Mailbox/index.js
+++ src/components/Mailbox/index.js
@@ -12,83 +12,88 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import API from "../../API";
 
 const Mailbox = () => {
-	let { mailbox } = useParams();
-	const navigate = useNavigate();
-	const [currentMailbox, setCurrentMailbox] = useState(mailbox);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(false);
-	const [messages, setMessages] = useState([]);
+  let { mailbox } = useParams();
+  const navigate = useNavigate();
+  const [currentMailbox, setCurrentMailbox] = useState(mailbox);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [messages, setMessages] = useState([]);
 
-	useEffect(() => {
-		const fetchMessages = async () => {
-			try {
-				setError(false);
-				setLoading(true);
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        setError(false);
+        setLoading(true);
 
-				const emails = await API.fetchMessages(mailbox);
+        const emails = await API.fetchMessages(mailbox);
 
-				setMessages(emails);
-			} catch (ex) {
-				setError(true);
-			}
-			setLoading(false);
-		};
-		fetchMessages(mailbox);
-	}, [mailbox]);
+        setMessages(emails);
+      } catch (ex) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchMessages(mailbox);
+  }, [mailbox]);
 
-	const handleClick = (id) => {
-		const email = messages.find((message) => message.id === id);
-		markAsRead(email);
+  const handleClick = (id) => {
+    const email = messages.find((message) => message.id === id);
+    markAsRead(email);
 
-		// pass the selected message through state props into the read email component
-		navigate("/read", { state: email });
-	};
+    // pass the selected message through state props into the read email component
+    navigate("/read", { state: email });
+  };
 
-	function markAsRead(email) {
-		email.read = true;
-		API.updateReadOrArchiveStatus(email);
-	}
-	// TODO add pagination
+  function markAsRead(email) {
+    console.log(email);
+    email.read = true;
+    API.updateReadOrArchiveStatus(email);
+  }
+  // TODO add pagination
 
-	return (
-		<Wrapper>
-			{loading ? (
-				<Spinner />
-			) : (
-				messages.map((message) => (
-					<MessageLine
-						key={message.id}
-						id={message.id}
-						sender={message.sender}
-						subject={message.subject}
-						read={message.read}
-						timestamp={message.timestamp}
-						handleClick={handleClick}
-					/>
-				))
-			)}
-			{messages.length < 1 && (
-				<div className="no-messages">You have no {mailbox} messages.</div>
-			)}
-		</Wrapper>
-	);
-	// return (
-	// 	<Wrapper>
-	// 		{messages.map((message) => (
-	//
-	// 			<MessageLine
-	// 				key={message.id}
-	// 				id={message.id}
-	// 				sender={message.sender}
-	// 				subject={message.subject}
-	// 				read={message.read}
-	// 				timestamp={message.timestamp}
-	// 				handleClick={handleClick}
-	// 			/>
-	//
-	// 		))}
-	// 	</Wrapper>
-	// );
+  return (
+    <Wrapper>
+      {loading ? (
+        <Spinner />
+      ) : (
+        messages.map((message) => (
+          <MessageLine
+            key={message.id}
+            id={message.id}
+            sender={
+              mailbox === "sent"
+                ? `To: ${message.recipients[0]}`
+                : message.sender
+            }
+            subject={message.subject}
+            read={message.read}
+            timestamp={message.timestamp}
+            handleClick={handleClick}
+          />
+        ))
+      )}
+      {messages.length < 1 && (
+        <div className="no-messages">You have no {mailbox} messages.</div>
+      )}
+    </Wrapper>
+  );
+  // return (
+  // 	<Wrapper>
+  // 		{messages.map((message) => (
+  //
+  // 			<MessageLine
+  // 				key={message.id}
+  // 				id={message.id}
+  // 				sender={message.sender}
+  // 				subject={message.subject}
+  // 				read={message.read}
+  // 				timestamp={message.timestamp}
+  // 				handleClick={handleClick}
+  // 			/>
+  //
+  // 		))}
+  // 	</Wrapper>
+  // );
 };
 
 export default Mailbox;
