d69c1a1e51952cca13cd9c7739979019ae372a51
Thom Zolghadr <thom@thomcodes.com>
2022-05-06 15:02:13 -0700

    Remove double margin on sibling MessageLines in inbox

--- src/components/Mailbox/Mailbox.styles.js
+++ src/components/Mailbox/Mailbox.styles.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
+import { Wrapper as MessageLine } from "../MessageLine/MessageLine.styles.js";
 
 export const Wrapper = styled.div`
-	max-width: var(--maxWidth);
-	margin: 0 auto;
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+
+  ${MessageLine} + ${MessageLine} {
+    margin-top: -1px;
+  }
 `;
