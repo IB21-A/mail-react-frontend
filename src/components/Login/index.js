43a6b7b36faf6d38888729a882646818d5f634fc
Thom Zolghadr <thom@thomcodes.com>
2022-05-06 21:33:55 -0700

    Add referrer check

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // Styles
 import { Wrapper } from "./Login.styles";
 // React components
@@ -8,67 +8,67 @@ import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 import API from "../../API";
 // hooks
-import {useAuth} from '../../hooks/useAuth';
+import { useAuth } from "../../hooks/useAuth";
 
 const Login = () => {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-    const [error, setError] = useState()
-    const navigate = useNavigate();
-    const auth = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState();
+  const navigate = useNavigate();
+  const auth = useAuth();
 
-	const doSubmit = async(e) => {
-        setError(false);
-        console.log('dosubmit');
-		e.preventDefault();
-        const user = await auth.login(username, password);
-        // const user = await API.login(username, password);
+  useEffect(() => {
+    console.log(document.referrer === "https://mail-thom.heroku.com");
+  });
 
-        if (!user) {
-            setError(true);
-        }
-        
-	};
+  const doSubmit = async (e) => {
+    setError(false);
+    console.log("dosubmit");
+    e.preventDefault();
+    const user = await auth.login(username, password);
 
+    if (!user) {
+      setError(true);
+    }
+  };
 
-    // if (API.getCurrentUser()) { navigate('/')};
-	return (
-		<Wrapper>
-			<h1>Login</h1>
-			<Form>
-				{error && (
-					<div className="error">
-						Username or Password incorrect. Try again.
-					</div>
-				)}
-				<Form.Group className="mb-3" controlId="formBasicEmail">
-					<Form.Label>Username</Form.Label>
-					<Form.Control
-						type="email"
-						placeholder="Enter Username"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-					<Form.Text className="text-muted">
-						The email address you are accessing
-					</Form.Text>
-				</Form.Group>
+  return (
+    <Wrapper>
+      <h1>Login</h1>
+      <Form>
+        {error && (
+          <div className="error">
+            Username or Password incorrect. Try again.
+          </div>
+        )}
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            The email address you are accessing
+          </Form.Text>
+        </Form.Group>
 
-				<Form.Group className="mb-3" controlId="formBasicPassword">
-					<Form.Label>Password</Form.Label>
-					<Form.Control
-						type="password"
-						placeholder="Password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</Form.Group>
-				<Button variant="primary" type="submit" onClick={doSubmit}>
-					Submit
-				</Button>
-			</Form>
-		</Wrapper>
-	);
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={doSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default Login;
